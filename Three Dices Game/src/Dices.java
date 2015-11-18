3bd535388ddbf6cae05e3f2bbb2c9161c4ae0aaa
SAC-CS112-NGUYEN-TUAN <tmacnguyen0525@gmail.com>
2015-11-17 23:46:37 -0800

    Initiate Homework Week 10.

---
+++ Three Dices Game/src/Dices.java
@@ -0,0 +1,14 @@
+
+public class Dices {
+		
+	int Dices(int max) {
+		
+		int computerNum;
+		computerNum = 0 + (int) (Math.random()*max);
+		
+		return computerNum;
+	}
+	
+	
+
+}
---
+++ Three Dices Game/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	Dices roll = new Dices();
+	int totalroll = (roll.Dices(6)+roll.Dices(12)+roll.Dices(18));
+	System.out.println(totalroll);
+	
+	}
+
+}
